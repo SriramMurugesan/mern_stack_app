767aa6f8d2b65dbbd3abc1d4170fb2a414d8ee73
SriramMurugesan <sri2014ram@gmail.com>
2026-04-17 22:17:25 +0530

    class

---
+++ class/task7/scripts.js
@@ -0,0 +1,11 @@
+console.log("Starting");
+
+setTimeout(() => {
+    console.log("2 seconds passed");
+}, 2000);
+
+console.log("Ending");
+
+
+//event loop: it is a process that keeps the javascript running
+//call stack: it is a stack that keeps the function calls
